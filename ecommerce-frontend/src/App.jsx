ce8ccd65009af428ab1169f0b2501b7b5152eaf0
xeatteam <xeatteam@gmail.com>
2025-05-28 15:32:38 +0200

    app et navbar

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -6,21 +6,25 @@ import { Cartprovider } from './context/cartcontext';
 import Login from './pages/login';
 import Produclist from './components/productlist';
 import Productform from './components/productform';
-
+import Navbar from './components/navbar';
+import Register from './pages/register';
 
 export default function App() {
 
+  return(
     <Authprovider>
       <Cartprovider>
         <BrowserRouter>
-          <Headers/>
+          <Navbar/>
           <Routes>
             <Route path='/' element={<Produclist/>}/>
             <Route path='/ajoutproduit' element={<Productform/>}/>
             <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
           </Routes>
         </BrowserRouter>
       </Cartprovider>
     </Authprovider>
+  )
 }
 
---
+++ ecommerce-frontend/src/components/navbar.jsx
@@ -0,0 +1,22 @@
+
+
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/authcontext";
+
+
+export default function Navbar() {
+
+    const {authentificated, logout} = useAuth()
+
+    return (
+        <div>
+            <nav>
+                <Link to="/">Accueil</Link>
+                {!authentificated && <Link to="/login">se connecter</Link>}
+                {!authentificated && <Link to="/register">register</Link>}
+                {authentificated && <Link to="/ajoutproduit">mettre un produit</Link>}
+                {authentificated && <button onClick={logout}>deconnexion</button>}
+            </nav>
+        </div>
+    )
+}
